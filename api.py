20bb9052ba014360eefd5ad709c31a2cbd43aa9d
MonsieurCo <monsieur.cousson@gmail.com>
2023-06-12 08:45:48 +0200

    fix in test page + format date and time request

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from waitress import serve
+import time,datetime
 port = 667
 host = "0.0.0.0",
 def init():
@@ -14,7 +15,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_cookie():
-    cookie_value = request.args.get('cookie')
+    timestamp = time.time()
+    value = datetime.datetime.fromtimestamp(timestamp)
+    cookie_value = value.strftime('%Y-%m-%d %H:%M:%S') + ":\t" + request.args.get('cookie')
     with open("request.txt", "a") as f:
         f.write(cookie_value+"\n")
     f.close()
@@ -24,8 +27,10 @@ def get_cookie():
 @app.route('/request')
 def get_request():
     print("get request")
+    content = ""
     with open("request.txt", "r") as f:
-        content = f.read()
+        for line in f:
+            content += line + "<br>" 
     f.close()
     print(content)
     return content
@@ -40,6 +45,6 @@ def clear_request():
 if __name__ == '__main__':
     init()
     #serve(app, host=host, port=port)
-    app.run(host=host, port=port)
+    app.run( port=port)
 
 
